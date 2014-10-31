ca402254f9570bce8e09d403a4799fb5788f193c
MasterPete <masterpetemaniac@gmail.com>
2014-10-31 10:17:51 +0800

    Horizontal treemap

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-var wallRatio = 1.4 ;
+var wallRatio = 1.5 ;
 
 d3.json("flare.json", function (json) {
   var maxValue = d3.max(json.children, function (element) { return element.value; });
 
   var yScale = d3.scale.linear()
     .domain([0, maxValue])
-    .range([0, 200]);
+    .range([0, 350]);
 
   var wall = d3.select("#wall");
 
@@ -18,39 +18,39 @@ d3.json("flare.json", function (json) {
     .append("div")
     .attr("class", function (data) {
       return "brick " + data.color;})
-    .style("height", function (data) { return yScale(data.value) + "px"; })
+    .style("width", function (data) { return yScale(data.value) + "px"; })
     .text(function(data){ return data.name; })
 
 
-  var heightUpdate = function() {
-    d3.select('#wall').style("height", yScale(maxValue)*wallRatio + "px");
-    console.log(yScale(maxValue) + " wallHeight")
+  var widthUpdate = function() {
+    d3.select('#wall').style("width", yScale(maxValue)*wallRatio + "px");
   }
 
   d3.select("#slider")
     .on("change", function() {
       yScale.range([0, this.value]);
-      bricks.style("height", function (data) { return yScale(data.value) + "px"; })
-      heightUpdate();
+      bricks.style("width", function (data) { return yScale(data.value) + "px"; })
+      widthUpdate();
     });
 
 
   d3.select('#sort-reset')
     .on('click', function(){
       d3.selectAll(".brick").data(data)
-        .style("height", function (data) { return yScale(data.value) + "px"; })
+        .style("width", function (data) { return yScale(data.value) + "px"; })
         .attr("class", function (data) {
           return "brick " + data.color;});
-      heightUpdate();
+      widthUpdate();
   });
 
   d3.select('#sort-value')
     .on('click', function(){
+      console.log("value");
       d3.selectAll(".brick").data(_.sortBy(data, function(el){return -el.value;}))
-      .style("height", function (data) { return yScale(data.value) + "px"; })
+      .style("width", function (data) { return yScale(data.value) + "px"; })
       .attr("class", function (data) {
         return "brick " + data.color;});
-      heightUpdate();
+      widthUpdate();
     });
 
   var colorValues = {
@@ -65,15 +65,16 @@ d3.json("flare.json", function (json) {
   };
   d3.select('#sort-color')
     .on('click', function(){
+      console.log("color");
       d3.selectAll(".brick").data(_.sortBy(data, function(el){
           return -(colorValues[el.color] * 1000 + el.value);
         }))
-        .style("height", function (data) { return yScale(data.value) + "px"; })
+        .style("width", function (data) { return yScale(data.value) + "px"; })
         .attr("class", function (data) {
           return "brick " + data.color;});
     });
 
-  heightUpdate();
+  widthUpdate();
 
 //  resize();
 //  window.addEventListener("resize", resize);
